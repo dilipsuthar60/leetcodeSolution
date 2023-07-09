5741dd739a4af41695bbef5f1cb67afc76175c64
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-07-09 18:05:00 +0530

    Time: 434 ms (16.67%), Space: 186.6 MB (60.00%) - LeetHub

---
+++ 2766-relocate-marbles/2766-relocate-marbles.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+        map<int,int>mp;
+        for(auto it:nums)
+        {
+            mp[it]=it;
+        }
+        for(int i=0;i<moveFrom.size();i++)
+        {
+            if(mp.find(moveFrom[i])!=mp.end())
+            {
+                mp.erase(moveFrom[i]);
+                mp[moveTo[i]]=moveTo[i];
+            }
+        }
+        vector<int>ans;
+        for(auto &[a,b]:mp)
+        {
+            ans.push_back(b);
+        }
+        return ans;
+    }
+};
