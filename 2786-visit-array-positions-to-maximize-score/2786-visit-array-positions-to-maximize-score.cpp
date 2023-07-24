bfb808af5d62b51b9ef4175183b1abb61abde531
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-07-24 13:41:48 +0530

    Time: 440 ms (6.67%), Space: 170.4 MB (6.67%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int n;
-    long long dp[500004][2];
+    vector<vector<long long>>dp;
     long long find(vector<int>&nums,int index,int prev,int x)
     {
         if(index>=n)
@@ -18,31 +18,31 @@ public:
         {
             if(prev)
             {
-                take=nums[index]+find(nums,index+1,1,x);
+                take=max(take,nums[index]+find(nums,index+1,1,x));
             }
             else
             {
-                take=nums[index]-x+find(nums,index+1,1,x);
+                take=max(take,nums[index]-x+find(nums,index+1,1,x));
             }
         }
         else
         {
             if(prev)
             {
-                take=nums[index]-x+find(nums,index+1,0,x);
+                take=max(take,nums[index]-x+find(nums,index+1,0,x));
             }
             else
             {
-                take=nums[index]+find(nums,index+1,0,x);
+                take=max(take,nums[index]+find(nums,index+1,0,x));
             }
         }
-        notTake=find(nums,index+1,prev,x);
+        notTake=max(notTake,find(nums,index+1,prev,x));
         return dp[index][prev]= max(take,notTake);
     }
     long long maxScore(vector<int>& nums, int x) 
     {
-        memset(dp,-1,sizeof(dp));
         n=nums.size();
+        dp=vector<vector<long long>>(n+1,vector<long long>(2,-1));
         int prev=nums[0]%2;
         return nums[0]+find(nums,1,prev,x);
     }
