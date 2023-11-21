5fc5fa75fe5cecc08bc183521a6aaaacf9843e28
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2023-11-21 08:41:34 +0530

    Time: 96 ms (62.92%), Space: 57 MB (83.57%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int find(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            sum=sum*10+n%10;
+            n=n/10;
+        }
+        return sum;
+    }
+    int countNicePairs(vector<int>& nums) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int val=find(nums[i]);
+            mp[nums[i]-val]++;
+        }
+        long long  ans=0;
+        const int m=1e9+7;
+        for(auto it:mp)
+        {
+            if(it.second>=2)
+            {
+                long long n=it.second;
+                ans=ans+((n%m)*((n-1)%m)/2)%m;
+            }
+        }
+        return ans%m;
+    }
+};
