b02eca1ddabc2972704566f8f65c65ff1299c4a5
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-02-04 00:40:30 -0800

    Time: 263 ms (100.00%), Space: 140.4 MB (40.00%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.cpp
@@ -3,29 +3,26 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) 
     {
         int n=nums.size();
-        vector<long long>prefix(n+1,0);
-        for(int i=0;i<n;i++)
-        {
-            prefix[i+1]=prefix[i]+nums[i];
-        }
+        long long sum=0;
         long long ans=-1e18;
         unordered_map<long long,long long>mp;
         for(int i=0;i<n;i++)
         {
+            if(mp.find(nums[i])==mp.end())
+            {
+                mp[nums[i]]=sum;
+            }
+            else{
+                mp[nums[i]]=min(mp[nums[i]],sum);
+            }
+            sum+=nums[i];
             if(mp.find(nums[i]-k)!=mp.end())
             {
-                ans=max(ans,prefix[i+1]-mp[nums[i]-k]);
+                ans=max(ans,sum-mp[nums[i]-k]);
             }
             if(mp.find(nums[i]+k)!=mp.end())
             {
-                 ans=max(ans,prefix[i+1]-mp[nums[i]+k]);
-            }
-            if(mp.find(nums[i])==mp.end())
-            {
-                mp[nums[i]]=prefix[i];
-            }
-            else{
-                mp[nums[i]]=min(mp[nums[i]],prefix[i]);
+                 ans=max(ans,sum-mp[nums[i]+k]);
             }
         }
         if(ans==-1e18)
