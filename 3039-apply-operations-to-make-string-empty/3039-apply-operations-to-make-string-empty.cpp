d16324af8297a2893534016ebc376e2646ac7dbb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-02-25 02:06:30 -0800

    Time: 279 ms (43.86%), Space: 111.9 MB (5.00%) - LeetHub

---
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string lastNonEmptyString(string s) 
+    {
+        int n=s.size();
+        unordered_map<char,vector<int>>mp;
+        int maxFrequency=0;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]].push_back(i);
+            maxFrequency=max(maxFrequency,(int)mp[s[i]].size());
+        }
+        vector<int>secondLastCharacterIndex;
+        for(auto [_,v]:mp)
+        {
+            if(v.size()==maxFrequency)
+            secondLastCharacterIndex.push_back(v.back());
+        }
+        sort(secondLastCharacterIndex.begin(),secondLastCharacterIndex.end());
+        string result="";
+        for(auto &index:secondLastCharacterIndex)
+        {
+            result+=s[index];
+        }
+        return result;
+    }
+};
