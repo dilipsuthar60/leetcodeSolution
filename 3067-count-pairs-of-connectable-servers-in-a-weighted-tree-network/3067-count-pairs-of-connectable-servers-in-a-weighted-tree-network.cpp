c3515489dbe617162faf5bde54883cb15d137362
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-03-03 00:00:52 -0800

    Time: 125 ms (63.64%), Space: 37.8 MB (72.73%) - LeetHub

---
+++ 3067-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3067-count-pairs-of-connectable-servers-in-a-weighted-tree-network.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int find(vector<vector<pair<int,int>>>&graph,int node,int currentWeightSum,int signalSpeed,int parent)
+    {
+        int count=0;
+        if(currentWeightSum%signalSpeed==0)
+        {
+            count++;
+        }
+        for(auto &[child,childWeight]:graph[node])
+        {
+            if(child!=parent)
+            {
+                count+=find(graph,child,currentWeightSum+childWeight,signalSpeed,node);
+            }
+        }
+        return count;
+    }
+    vector<int> countPairsOfConnectableServers(vector<vector<int>>& edges, int signalSpeed) 
+    {  
+        int size=edges.size();
+        vector<vector<pair<int,int>>>graph(size+10);
+        for(auto &edge:edges)
+        {
+            graph[edge[0]].push_back({edge[1],edge[2]});
+            graph[edge[1]].push_back({edge[0],edge[2]});
+        }
+        vector<int>result(size+1);
+        for(int index=0;index<=size;index++)
+        {
+            vector<int>pathCount;
+            for(auto it:graph[index])
+            {
+                int count=find(graph,it.first,it.second,signalSpeed,index);
+                pathCount.push_back(count);
+            }
+            for(int i=0;i<pathCount.size()-1;i++)
+            {
+                for(int j=i+1;j<pathCount.size();j++)
+                {
+                    result[index]+=pathCount[i]*pathCount[j];
+                }
+            }
+        }
+        return result;
+    }
+};
