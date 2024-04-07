c49d84316618d491856915fef98734c580ea6c93
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-04-07 08:33:40 -0700

    Time: 409 ms (51.64%), Space: 819.3 MB (46.42%) - LeetHub

---
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    class node{
+        public:
+        node*child[26]={NULL};
+        pair<int,int>pt ={1e9,1e9};
+    };
+    node*root;
+    void insert(string&s,int postion)
+    {
+        node*current=root;
+        int len=s.size();
+        current->pt=min(current->pt,{len,postion});
+        for(int i=len-1;i>=0;i--)
+        {
+            int index=s[i]-'a';
+            if(current->child[index]==NULL)
+            {
+                current->child[index]=new node();
+            }
+            current=current->child[index];
+            current->pt=min(current->pt,{len,postion});
+        }
+    }
+    int find(string&s)
+    {
+        int n=s.size();
+        node*current=root;
+        for(int i=n-1;i>=0;i--)
+        {
+            int index=s[i]-'a';
+            if(current->child[index]==NULL)
+            {
+                return current->pt.second;
+            }
+            current=current->child[index];
+        }
+        return current->pt.second;;
+    }
+    vector<int> stringIndices(vector<string>&word, vector<string>&query) 
+    {
+        root=new node();
+        for(int i=0;i<word.size();i++)
+        {
+            insert(word[i],i);
+        }
+        vector<int>result;
+        for(int i=0;i<query.size();i++)
+        {
+            result.push_back(find(query[i]));
+        }
+        return result;
+    }
+};
