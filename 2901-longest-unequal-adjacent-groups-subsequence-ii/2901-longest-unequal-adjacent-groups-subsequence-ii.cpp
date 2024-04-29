3bbc9910e0081dc08fa2e9fa500a596d27188b08
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-04-29 10:26:40 -0700

    Time: 42 ms (97.44%), Space: 32.7 MB (85.47%) - LeetHub

--- 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
+++ 2901-longest-unequal-adjacent-groups-subsequence-ii/2901-longest-unequal-adjacent-groups-subsequence-ii.cpp
@@ -13,29 +13,37 @@ public:
     }
     vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
         int n=words.size();
-        vector<vector<string>>dp(n);
         int maxElement=0;
+        vector<int>dp(n,1);
+        vector<int>parent(n,-1);
+        
         for(int i=0;i<n;i++)
         {
-            dp[i].push_back(words[i]);
             for(int j=0;j<i;j++)
             {
-                if(hammingDistance(words[i],words[j])&&groups[i]!=groups[j]&&dp[j].size()+1>dp[i].size())
+                if(hammingDistance(words[i],words[j])&&groups[i]!=groups[j]&&dp[j]+1>dp[i])
                 {
-                    dp[i]=dp[j];
-                    dp[i].push_back(words[i]);
+                    dp[i]=dp[j]+1;
+                    parent[i]=j;
                 }
             }
-            maxElement=max(maxElement,(int)dp[i].size());
+            maxElement=max(maxElement,(int)dp[i]);
         }
+        int maxIndex=-1;
         for(int i=0;i<n;i++)
         {
-            if(maxElement==dp[i].size())
+            if(maxElement==dp[i])
             {
-                return dp[i];
+                maxIndex=i;
             }
         }
-        // cout<<value<<endl;
-        return {};
+        vector<string>result;
+        while(maxIndex!=-1)
+        {
+            result.push_back(words[maxIndex]);
+            maxIndex=parent[maxIndex];
+        }
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
