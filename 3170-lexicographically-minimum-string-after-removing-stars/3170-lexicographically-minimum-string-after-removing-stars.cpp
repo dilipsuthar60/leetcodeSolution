876e7eaaee6a0e01a6a9460109fa780f8ea2d11d
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-02 18:10:18 +0530

    Time: 313 ms (66.67%), Space: 111.4 MB (16.67%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     string clearStars(string s) {
         int n=s.size();
-        unordered_map<int,bool>removeIndex;
+        vector<bool>visited(n,true);
         set<pair<char,int>>st;
         for(int i=0;i<n;i++){
             if(s[i]=='*'&&st.size()){
                 auto [ch,index]=*st.begin();
                 st.erase(st.begin());
-                removeIndex[abs(index)]=true;
+                visited[-index]=false;
             }
             else{
                 st.insert({s[i],-i});
@@ -17,7 +17,7 @@ public:
         string result="";
         for(int i=0;i<n;i++)
         {
-            if(s[i]!='*'&&removeIndex.find(i)==removeIndex.end()){
+            if(s[i]!='*'&&visited[i]){
                 result+=s[i];
             }
         }
