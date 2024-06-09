08763cedfacd567ede5fc6628901d8248dd674ca
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-09 16:55:48 +0530

    Time: 111 ms (88.38%), Space: 112.5 MB (17.65%) - LeetHub

--- 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
@@ -5,11 +5,12 @@ public:
         int sizeOfDigit=to_string(nums.front()).size();
         long long map[sizeOfDigit][10];
         memset(map,0,sizeof(map));
-        for(auto &it:nums){
-            string s=to_string(it);
-            for(int i=0;i<s.size();i++)
+        for(int i=0;i<sizeOfDigit;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                map[i][s[i]-'0']++;
+                map[i][nums[j]%10]++;
+                nums[j]/=10;
             }
         }
         long long result=0;
