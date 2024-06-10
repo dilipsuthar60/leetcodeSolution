f40517dc7ac0ee7093bd0ce6ec6466c0b8f2b93e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-10 23:22:28 +0530

    Time: 59 ms (97.42%), Space: 15.6 MB (100.00%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -3,12 +3,12 @@ public:
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
         bitset<5005>dp;
         dp[0]=1;
-        for(int i=0;i<mat.size();i++)
+        for(auto &col:mat)
         {
             bitset<5005>temp;
-            for(int j=0;j<mat[0].size();j++)
+            for(auto &item:col)
             {
-                temp=temp|(dp<<mat[i][j]);
+                temp=temp|(dp<<item);
             }
             dp=temp;
         }
