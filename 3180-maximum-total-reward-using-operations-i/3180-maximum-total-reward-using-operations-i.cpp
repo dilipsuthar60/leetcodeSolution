e0da1b4ec6fe38fb41c117f7f3276d816bbb39a2
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-10 23:42:13 +0530

    Time: 535 ms (20.00%), Space: 25.9 MB (60.00%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    int dp[2001][4001];
-    int find(vector<int>&nums,int index,int sum=0){
-        if(index>=nums.size()) return 0;
-        if(dp[index][sum]!=-1) return dp[index][sum];
-        int result=find(nums,index+1,sum);
-        if(sum<nums[index]){
-            result=max(result,nums[index]+find(nums,index+1,sum+nums[index]));
-        }
-        return dp[index][sum] = result;
-    }
     int maxTotalReward(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
         set<int>s;
         for(int i=0;i<n;i++)
         {
-            for(auto it:s){
+            for(auto &it:s){
                 if(it>=nums[i]) break;
                 s.insert(nums[i]+it);
             }
