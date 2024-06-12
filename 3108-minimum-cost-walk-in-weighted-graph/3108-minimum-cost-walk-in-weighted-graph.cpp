84454db7853c9ecb25a0286f252cbbd4d7c18b2b
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-12 23:37:29 +0530

    Time: 524 ms (31.08%), Space: 229.2 MB (24.69%) - LeetHub

---
+++ 3108-minimum-cost-walk-in-weighted-graph/3108-minimum-cost-walk-in-weighted-graph.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int>parent;
+    vector<int>weight;
+    int find(int x,int y){
+        if(getParent(x)!=getParent(y)) return -1;
+        return weight[getParent(x)];
+    }
+    void merge(int x,int y,int wt){
+        x=getParent(x);
+        y=getParent(y);
+        weight[x]=weight[y]=(wt&weight[x]&weight[y]);
+        parent[x]=y;
+    }
+    int getParent(int x){
+        if(parent[x]==x) return x;
+        return parent[x] = getParent(parent[x]);
+    }
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& querys) {
+        weight=vector<int>(n+10,(1<<30)-1);
+        parent=vector<int>(n+10);
+        for(int i=0;i<n;i++) parent[i]=i;
+        for(auto edge:edges){
+            merge(edge[0],edge[1],edge[2]);
+        }
+        vector<int>result;
+        for(auto query:querys){
+            result.push_back(find(query[0],query[1]));
+        }
+        return result;
+    }
+};
