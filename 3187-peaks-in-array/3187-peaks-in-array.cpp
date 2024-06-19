9aece9fcebe600c1dcd66ab83509c704e07e4d51
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-06-19 23:33:56 +0530

    Time: 555 ms (76.39%), Space: 250 MB (38.59%) - LeetHub

---
+++ 3187-peaks-in-array/3187-peaks-in-array.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int bit[100005]={0};
+    const int N=100005;
+    bool isPeak(int index,vector<int>&nums)
+    {
+        return index>0&&index+1<nums.size()&&nums[index-1]<nums[index]&&nums[index]>nums[index+1];
+    }
+    void update(int i,int value){
+        i++;
+        while(i<N){
+            bit[i]+=value;
+            i+=(i&-i);
+        }
+    }
+    int find(int i){
+        i++;
+        int sum=0;
+        while(i>0){
+            sum+=bit[i];
+            i-=(i&-i);
+        }
+        return sum;
+    }
+    vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(isPeak(i,nums)){
+                cout<<"peak"<<" "<<index<<endl;
+                update(i,1);
+            }
+        }
+        vector<int>result;
+        for(auto query:queries){
+            int type=query[0];
+            if(type==1){
+                int l=query[1];
+                int r=query[2];
+                result.push_back(max(0,find(r-1)-find(l)));
+            }else{
+                int index=query[1];
+                int value=query[2];
+                for(int i=index-1;i<=index+1;i++)
+                {
+                    if(isPeak(i,nums)){
+                       update(i,-1);   
+                    }
+                }
+                nums[index]=value;
+                for(int i=index-1;i<=index+1;i++)
+                {
+                    if(isPeak(i,nums)){
+                        update(i,1);   
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
