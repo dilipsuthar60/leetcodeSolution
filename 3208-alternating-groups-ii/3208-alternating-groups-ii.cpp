5aef84eca17feeb406cdf0c1cb7f0c64af0c1d20
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-07-06 21:53:07 +0530

    Time: 66 ms (28.57%), Space: 101 MB (14.29%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -1,57 +1,14 @@
 class Solution {
 public:
-    vector<int>kmp(string s,string p){
-        s=p+"###"+s;
-        int n=s.size();
-        vector<int>result(n,0);
-        int i=1;
-        int len=0;
-        while(i<n){
-            if(s[i]==s[len]){
-                result[i]=++len;
-                i++;
-            }
-            else{
-                if(len){
-                    len=result[len-1];
-                }
-                else{
-                    i++;
-                }
-            }
-        }
-        return result;
-    }
     int numberOfAlternatingGroups(vector<int>& nums, int k) {
-        for(int i=0;i<k-1;i++){
-            nums.push_back(nums[i]);
+        int missMatch=0;
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n+k-1;i++){
+            if(nums[i%n]!=nums[(i+1)%n]) missMatch++;
+            else missMatch=1;
+            if(missMatch>=k) ans++;
         }
-        string s="";
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) s.push_back('0');
-            if(nums[i]==1) s.push_back('1');
-        }
-        string p1="";
-        string p2="";
-        for(int i=0;i<k;i++){
-            if(i%2==0){
-                p1.push_back('0');
-                p2.push_back('1');
-            }
-            else{
-                p1.push_back('1');
-                p2.push_back('0');
-            }
-        }
-        int count=0;
-        vector<int>result1=kmp(s,p1);
-        vector<int>result2=kmp(s,p2);
-        
-        for(int i=0;i<result2.size();i++){
-            if(result1[i]==k||result2[i]==k){
-                count++;
-            }
-        }
-        return count;
+        return ans;
     }
 };
