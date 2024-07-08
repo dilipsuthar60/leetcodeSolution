4df8518b01bf5a3b63e96d9081d737b02439e426
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-07-08 23:19:13 +0530

    Time: 1193 ms (30.16%), Space: 339.5 MB (13.00%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
@@ -6,7 +6,7 @@ public:
         unordered_set<int>distict;
         for(int i=0;i<n;i++){
             unordered_set<int>current;
-            for(auto it:prev){
+            for(auto &it:prev){
                 current.insert(nums[i]|it);
             }
             current.insert(nums[i]);
