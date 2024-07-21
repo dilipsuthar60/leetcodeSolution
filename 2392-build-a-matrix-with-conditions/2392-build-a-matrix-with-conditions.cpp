fc5e89d5bff72a2828708c5affefeaca7dfc2875
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-07-21 22:27:25 +0530

    Time: 97 ms (70.42%), Space: 60.7 MB (76.90%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,54 +1,56 @@
-class Solution {
-public:
-    vector<int>find(vector<vector<int>>&mat,int k)
-    {
-        vector<int>ans;
-        vector<int>ind(k+1,0);
-        vector<vector<int>>dp(k+1);
-        for(auto &it:mat)
+class Solution
+{
+    public:
+        vector<int> find(vector<vector < int>> &mat, int k)
         {
-            dp[it[0]].push_back(it[1]);
-            ind[it[1]]++;
-        }
-        queue<int>q;
-        for(int i=1;i<=k;i++)
-        {
-            if(ind[i]==0)
+            vector<int> ans;
+            vector<int> ind(k + 1, 0);
+            vector<vector < int>> dp(k + 1);
+            for (auto &it: mat)
             {
-                q.push(i);
+                dp[it[0]].push_back(it[1]);
+                ind[it[1]]++;
             }
-        }
-        while(q.size())
-        {
-            auto temp=q.front();
-            q.pop();
-            ans.push_back(temp);
-            for(auto it:dp[temp])
+            queue<int> q;
+            for (int i = 1; i <= k; i++)
             {
-                if(--ind[it]==0)
+                if (ind[i] == 0)
                 {
-                    q.push(it);
+                    q.push(i);
                 }
             }
+            while (q.size())
+            {
+                auto temp = q.front();
+                q.pop();
+                ans.push_back(temp);
+                for (auto it: dp[temp])
+                {
+                    if (--ind[it] == 0)
+                    {
+                        q.push(it);
+                    }
+                }
+            }
+            return ans;
         }
-        return ans;
-    }
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& row, vector<vector<int>>& col) {
-        vector<int>Row=find(row,k);
-        vector<int>Col=find(col,k);
-        if(Row.size()!=k||Col.size()!=k)
+    vector<vector < int>> buildMatrix(int k, vector<vector < int>> &row, vector< vector< int>> &col)
+    {
+        vector<int> Row = find(row, k);
+        vector<int> Col = find(col, k);
+        if (Row.size() != k || Col.size() != k)
         {
             return {};
         }
-        unordered_map<int,int>mp;
-        for(int i=0;i<k;i++)
+        unordered_map<int, int> mp;
+        for (int i = 0; i < k; i++)
         {
-            mp[Col[i]]=i;
+            mp[Col[i]] = i;
         }
-        vector<vector<int>>ans(k,vector<int>(k,0));
-        for(int i=0;i<k;i++)
+        vector<vector < int>> ans(k, vector<int> (k, 0));
+        for (int i = 0; i < k; i++)
         {
-            ans[i][mp[Row[i]]]=Row[i];
+            ans[i][mp[Row[i]]] = Row[i];
         }
         return ans;
     }
