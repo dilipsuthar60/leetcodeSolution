687443bb9fb383ecfe6b1e085db80ede7d7bd6fb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-07-23 23:10:34 +0530

    Time: 48 ms (71.80%), Space: 94.5 MB (73.09%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int find(vector<vector<int>>&nums,int n,int m,int i,int j){
+        if(i<0||j<0||i>=n||j>=m||nums[i][j]==0) return 0;
+        int res=nums[i][j];
+        nums[i][j]=0;
+        int op1=find(nums,n,m,i+1,j);
+        int op2=find(nums,n,m,i-1,j);
+        int op3=find(nums,n,m,i,j+1);
+        int op4=find(nums,n,m,i,j-1);
+        return res+op1+op2+op3+op4;
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int maxFish=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]){
+                    maxFish=max(maxFish,find(grid,n,m,i,j));
+                }
+            }
+        }
+        return maxFish;
+    }
+};
