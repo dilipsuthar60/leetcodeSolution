2908c2df23242b61b5c74fe8765486420705d6b3
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-07-31 23:05:19 +0530

    Time: 602 ms (59.87%), Space: 165 MB (97.12%) - LeetHub

---
+++ 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<pair<int,int>>dp[100005];
+    int dfs1(int node,vector<int>&vis){
+        int sum=0;
+        vis[node]=1;
+        for(auto &it:dp[node]){
+            if(!vis[it.first])
+            sum+=it.second+dfs1(it.first,vis);
+        }
+        return sum;
+    }
+    void dfs2(int node,vector<int>&ans){
+        for(auto &it:dp[node]){
+            if(ans[it.first]==-1){
+                ans[it.first]=ans[node]+(it.second==1?-1:1);
+                dfs2(it.first,ans);
+            }
+        }
+    }
+    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+        for(auto &it:edges){
+            dp[it[0]].push_back({it[1],0});
+            dp[it[1]].push_back({it[0],1});
+        }
+        vector<int>vis(n+1,0);
+        int sum=dfs1(0,vis);
+        vector<int>ans(n,-1);
+        ans[0]=sum;
+        dfs2(0,ans);
+        return ans;
+    }
+};
