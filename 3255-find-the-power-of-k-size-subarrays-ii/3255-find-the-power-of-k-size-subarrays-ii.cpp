abe0487e76db2b9d85d382b48f668d84c80f56fe
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-08-17 21:46:00 +0530

    Time: 204 ms (100.00%), Space: 189.2 MB (100.00%) - LeetHub

--- 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
+++ 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int count=1;
-        int index=0;
-        vector<int>ans(n-k+1,-1);
-        for(int i=1;i<k;i++){
+        vector<int>conseutiveLength(n,1);
+        vector<int>ans;
+        for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1]+1){
-                count++;
-            }
-            else{
-                count=1;
+                conseutiveLength[i]+=conseutiveLength[i-1];
             }
         }
-        if(count>=k) ans[index]=nums[k-1];
-        index++;
-        for(int i=k;i<n;i++){
-            if(nums[i]==nums[i-1]+1){
-                count++;
+        for(int i=k-1;i<n;i++){
+            if(conseutiveLength[i]>=k){
+                ans.push_back(nums[i]);
             }
             else{
-                count=1;
+                ans.push_back(-1);
             }
-            if(count>=k) ans[index]=nums[i];
-            index++;
         }
         return ans;
     }
