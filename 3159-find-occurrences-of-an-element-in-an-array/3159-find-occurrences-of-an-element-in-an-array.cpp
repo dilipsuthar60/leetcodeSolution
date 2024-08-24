360a64ee9ceb44b503090c6a95628627dc2b09e4
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-08-25 00:17:19 +0530

    Time: 165 ms (69.60%), Space: 179.5 MB (92.95%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
         int n=nums.size();
-        unordered_map<int,vector<int>>mp;
+        vector<int>storeIndexes;
         int index=0;
-        for(auto &it:nums) mp[it].push_back(index++);
-        vector<int>ans;
+        for(auto &it:nums) {
+            if(it==x){
+                storeIndexes.push_back(index);
+            }
+            index++;
+        }
+        vector<int>ans((int)queries.size());
+        index=0;
         for(auto &query:queries){
-            if(mp[x].size()<query){
-                ans.push_back(-1);
-            }
-            else{
-                ans.push_back(mp[x][query-1]);
-            }
+            ans[index++]=(storeIndexes.size()<query)?-1:storeIndexes[query-1];
         }
         return ans;
     }
