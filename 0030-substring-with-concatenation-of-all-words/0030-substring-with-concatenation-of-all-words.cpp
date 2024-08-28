c6688ef6a6d68e6873b9f9fc6eb39e8e75bd3265
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-08-28 23:54:03 +0530

    Time: 2955 ms (5.04%), Space: 422.8 MB (5.02%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int k;
+    bool find(string &s,int &window,unordered_map<string,int>mp)
+    {
+        for(int i=0;i<s.size();i+=window)
+        {
+            string str=s.substr(i,window);
+            if(--mp[str]==-1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<int> findSubstring(string s, vector<string>& words) 
+    {
+        unordered_map<string,int>mp,new_map;
+        int first_len=words[0].size();
+        if(first_len>s.size()) return {};
+        k=words[0].size()*(words.size());
+        for(auto &it:words)
+        {
+            mp[it]++;
+        }
+        vector<int>ans;
+        for(int i=0;i<s.size()-k+1;i++)
+        {
+            string str=s.substr(i,k);
+            if(find(str,first_len,mp))
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
