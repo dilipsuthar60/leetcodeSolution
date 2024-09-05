248aebf355351454a4b23a5803d842549f4a88f8
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-05 23:25:29 +0530

    Time: 673 ms (45.58%), Space: 388.2 MB (53.62%) - LeetHub

---
+++ 3277-maximum-xor-score-subarray-queries/3277-maximum-xor-score-subarray-queries.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> maximumSubarrayXor(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;j++,i++){
+                if(gap==0){
+                    dp[i][j]=nums[i];
+                }
+                else if(gap==1){
+                    dp[i][j]=nums[i]^nums[j];
+                }
+                else{
+                    dp[i][j]=dp[i+1][j]^dp[i][j-1];
+                }
+            }
+        }
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;j++,i++){
+                if(gap==1){
+                    dp[i][j]=max({dp[i][j],nums[i],nums[j]});
+                }
+                else if(gap>1){
+                    dp[i][j]=max({dp[i][j],dp[i+1][j],dp[i][j-1]});
+                }
+            }
+        }
+        vector<int>ans;
+        for(auto &it:queries){
+            ans.push_back(dp[it[0]][it[1]]);
+        }
+        return ans;
+    }
+};
