21281fb20f46f0972223de9fd4044df205f0906a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-14 00:00:26 +0530

    Time: 181 ms (21.40%), Space: 86 MB (35.37%) - LeetHub

--- 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int getLowerBound(vector<int>&nums, int value){
+        int l=0;
+        int r=nums.size()-1;
+        int mid=0;
+        int index=nums.size();
+        while(l<=r){
+            mid=(l+r)/2;
+            if(nums[mid]>value){
+                index=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return index;
+    }
     vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
@@ -9,7 +26,7 @@ public:
         }
         vector<long long>ans;
         for(auto &value:queries){
-            long long index=lower_bound(nums.begin(),nums.end(),value)-nums.begin();
+            long long index=getLowerBound(nums,value);
             long long leftSum = value*index - (prefix[index]-prefix[0]);
             long long rightSum = (prefix.back()-prefix[index]) - value*(n-index);
             ans.push_back(leftSum+rightSum);
