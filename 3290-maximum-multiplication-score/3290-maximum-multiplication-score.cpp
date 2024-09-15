30db1d13bbcc489e6c2bf1712a8acee9daae326c
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-15 16:44:41 +0530

    Time: 310 ms (33.33%), Space: 202.2 MB (33.33%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    long long dp[4][100005];
-    long long find(vector<int>&a,vector<int>&b,int i,int j){
-        if(i>=a.size()) return 0;
-        if(j>=b.size()) return -1e18;
-        if(dp[i][j]!=-1) return dp[i][j];
-        long long take= 1ll*a[i]*b[j]+find(a,b,i+1,j+1);   
-        long long notTake = find(a,b,i,j+1);
-        return dp[i][j] = max(take,notTake);
-    }
     long long maxScore(vector<int>& a, vector<int>& b) {
-        memset(dp,-1,sizeof(dp));
-        return find(a,b,0,0);
+        vector<long long>dp(5,-1e18);
+        dp[0]=0;
+        for(int i=0;i<b.size();i++){
+            vector<long long>ndp = dp;
+            for(int j=0;j<4;j++){
+                ndp[j+1]=max(ndp[j+1],dp[j]+1ll*a[j]*b[i]);
+            }
+            swap(dp,ndp);
+        }
+        return dp.back();
     }
 };
