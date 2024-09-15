fba314cb71b79b86da54d61bd48a0a60a8c3f82c
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-15 16:44:53 +0530

    Time: 313 ms (33.33%), Space: 202.3 MB (33.33%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maxScore(vector<int>& a, vector<int>& b) {
-        vector<long long>dp(5,-1e18);
+        vector<long long>dp(5,-1e15);
         dp[0]=0;
         for(int i=0;i<b.size();i++){
             vector<long long>ndp = dp;
