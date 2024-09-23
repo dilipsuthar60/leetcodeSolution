9d601fb3aa9402db86b60ca2978ca51bb3527776
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-23 23:41:27 +0530

    Time: 204 ms (100.00%), Space: 32.4 MB (100.00%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     long long minNumberOfSeconds(int height, vector<int>& worker) {
-        set<tuple<long long,int,int>>s;
+        priority_queue<tuple<long long,int,int>,vector<tuple<long long,int,int>>,greater<tuple<long long,int,int>>>pq;
         for(int i=0;i<worker.size();i++){
-            s.insert({worker[i],1,i});
+            pq.push({worker[i],1,i});
         }
         long long ans=0;
         while(height--){
-            auto [cost,currentValue,index]=*s.begin();
-            s.erase(s.begin());
+            auto [cost,currentValue,index]=pq.top();
+            pq.pop();
             ans=max(ans,cost);
-            s.insert({cost+1ll*worker[index]*(currentValue+1),currentValue+1,index});
+            pq.push({cost+1ll*worker[index]*(currentValue+1),currentValue+1,index});
         }
         return ans;
     }
