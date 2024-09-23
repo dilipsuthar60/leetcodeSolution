d8aba2167baa1d3386ad7ed94ec6bceabd815015
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-09-23 23:33:09 +0530

    Time: 341 ms (100.00%), Space: 128.1 MB (100.00%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    bool find(vector<int>&nums,long long height,long long time){
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            long long l=0;
-            long long r=1e6;
-            long long reduce=0;
-            while(l<=r){
-                long long mid=(l+r)/2;
-                if(1ll*nums[i]*(mid*(mid+1)/2)<=time){
-                    reduce=mid;
-                    l=mid+1;
-                }
-                else{
-                    r=mid-1;
-                }
-            }
-            height-=reduce;
-        }
-        return height<=0;
-    }
     long long minNumberOfSeconds(int height, vector<int>& worker) {
-        long long l=0;
-        long long r=1e18;
+        set<tuple<long long,int,int>>s;
+        for(int i=0;i<worker.size();i++){
+            s.insert({worker[i],1,i});
+        }
         long long ans=0;
-        while(l<=r){
-            long long mid=(l+r)/2;
-            if(find(worker,height,mid)){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
+        while(height--){
+            auto [cost,currentValue,index]=*s.begin();
+            s.erase(s.begin());
+            ans=max(ans,cost);
+            s.insert({cost+1ll*worker[index]*(currentValue+1),currentValue+1,index});
         }
         return ans;
     }
