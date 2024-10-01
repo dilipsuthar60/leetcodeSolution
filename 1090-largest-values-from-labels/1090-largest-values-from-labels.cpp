ea48f6570173dd2d1cb2f1508ee2876b457ed06a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-10-02 00:15:19 +0530

    Time: 20 ms (64.68%), Space: 27.7 MB (17.91%) - LeetHub

--- 1090-largest-values-from-labels/1090-largest-values-from-labels.cpp
+++ 1090-largest-values-from-labels/1090-largest-values-from-labels.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int largestValsFromLabels(vector<int>& values, vector<int>& labels, int numWanted, int useLimit) {
-        map<int,multiset<int>>mp;
-        for(int i=0;i<values.size();i++){
-            mp[labels[i]].insert(values[i]);
-            if(mp[labels[i]].size()>useLimit){
-                mp[labels[i]].erase(mp[labels[i]].begin());
-            }
+        vector<pair<int,int>>v;
+        for(int i=0;i<labels.size();i++){
+            v.push_back({values[i],labels[i]});
         }
+        sort(v.rbegin(),v.rend());
+        unordered_map<int,int>mp;
         int ans=0;
-        vector<int>v;
-        for(auto &[_,st]:mp){
-            for(auto &it:st){
-                v.push_back(it);
+        for(int i=0,count=0;i<v.size()&&count<numWanted;i++){
+            if(++mp[v[i].second]<=useLimit){
+                ans+=v[i].first;
+                count++;
             }
-        }
-        sort(v.begin(),v.end());
-        int index=0;
-        while(v.size()&&index<numWanted){
-            ans+=v.back();
-            v.pop_back();
-            index++;
         }
         return ans;
     }
