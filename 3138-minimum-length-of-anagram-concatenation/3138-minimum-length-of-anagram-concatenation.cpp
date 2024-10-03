f503b4d7c1e8bdb1c8bbbc6e9dfc4a4c53765cd6
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-10-03 23:00:21 +0530

    Time: 271 ms (38.66%), Space: 180 MB (11.34%) - LeetHub

---
+++ 3138-minimum-length-of-anagram-concatenation/3138-minimum-length-of-anagram-concatenation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isAnagram(string &s1,string &s2){
+        vector<int>frequency(26,0);
+        for(auto &ch:s1) frequency[ch-'a']++;
+        for(auto &ch:s2) frequency[ch-'a']--;
+        for(int i=0;i<26;i++){
+            if(frequency[i]!=0) return false;
+        }
+        return true;
+    }
+    bool find(string &s,int k){
+        int n=s.size();
+        string first=s.substr(0,k);
+        for(int i=0;i<n;i+=k){
+            string current=s.substr(i,k);
+            if(!isAnagram(first,current)){
+                return false;
+            }
+        }
+        return true;
+    }
+    int minAnagramLength(string s) {
+        int n=s.size();
+        int ans=1e8;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                if(find(s,i)){
+                    ans=min(ans,i);
+                }
+                if(n!=i*i){
+                    if(find(s,n/i)){
+                        ans=min(ans,n/i);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
