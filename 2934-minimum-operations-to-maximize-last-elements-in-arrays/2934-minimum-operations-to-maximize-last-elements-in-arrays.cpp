3efb6d89b552c77f89630202f228003046e2e385
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-10-05 22:19:27 +0530

    Time: 27 ms (47.94%), Space: 46.7 MB (15.46%) - LeetHub

--- 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int dp[1001];
     int find(vector<int>&nums1,vector<int>&nums2,int index){
         if(index>=nums1.size()) return 0;
         int ans=1e8;
         if(nums1[index]<=nums1.back()&&nums2[index]<=nums2.back()){
             ans=min(ans,find(nums1,nums2,index+1));
-            return dp[index] = ans;
+            return ans;
         }
         if(nums2[index]<=nums1.back()&&nums1[index]<=nums2.back()){
             ans=min(ans,1+find(nums1,nums2,index+1));
-            return dp[index] = ans;
+            return ans;
         }
-        return dp[index] = ans;
+        return ans;
     }
     int minOperations(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp,-1,sizeof(dp));
         int notSwapLast=find(nums1,nums2,0);
         swap(nums1.back(),nums2.back());
-        // memset(dp,-1,sizeof(dp));
         int swapLast=1+find(nums1,nums2,0);
         int ans=min(notSwapLast,swapLast);
         return ans>=1e8?-1:ans;
