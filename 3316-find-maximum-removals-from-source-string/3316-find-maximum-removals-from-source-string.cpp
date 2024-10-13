74f4c77a0bc19ed7325b5a27432a1d1f621f77ec
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-10-13 18:42:15 +0530

    Time: 2490 ms (9.09%), Space: 66.4 MB (13.64%) - LeetHub

--- 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
+++ 3316-find-maximum-removals-from-source-string/3316-find-maximum-removals-from-source-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    unordered_set<int>vis;
+    int vis[3002];
     int dp[3001][3001];
     int find(string &s,string &p,int i,int j){
         if(i>=s.size()) return j==p.size()?0:-1e8;
         if(dp[i][j]!=-1) return dp[i][j];
         int ans=find(s,p,i+1,j);
-        if(vis.find(i)!=vis.end()){
+        if(vis[i]){
             ans=max(ans,1+find(s,p,i+1,j));
         }
         if(j<p.size()&&s[i]==p[j]){
@@ -15,7 +15,7 @@ public:
         return dp[i][j]= ans;
     }
     int maxRemovals(string source, string pattern, vector<int>& targetIndices){
-        for(auto &it:targetIndices) vis.insert(it);
+        for(auto &it:targetIndices) vis[it]=1;
         memset(dp,-1,sizeof(dp));
         return find(source,pattern,0,0);
     }
