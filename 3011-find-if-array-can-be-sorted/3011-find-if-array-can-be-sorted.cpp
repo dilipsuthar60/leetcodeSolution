2218db5e50524a65fcae323761df341d17276272
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-11-06 10:37:36 +0530

    Time: 12 ms (15.11%), Space: 31.2 MB (86.44%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int getBitCount(int n){
+    int getSetBits(int n){
         return __builtin_popcount(n);
     }
     bool canSortArray(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++)
-            {
-                if(getBitCount(nums[j])==getBitCount(nums[j-1])&&nums[j]<nums[j-1]){
+            for(int j=1;j<n;j++){
+                if(getSetBits(nums[j])==getSetBits(nums[j-1])&&nums[j]<nums[j-1]){
                     swap(nums[j],nums[j-1]);
                 }
             }
         }
-        if(is_sorted(nums.begin(),nums.end())) return true;
-        return false;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]) return false;
+        }
+        return true;
     }
 };
