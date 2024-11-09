a065b146043cf4fcfc4b013d5bcd84a6327bdb94
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-11-09 16:36:50 +0530

    Time: 65 ms (66.67%), Space: 126.8 MB (22.07%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]%modulo==k){
-                nums[i]=1;
-            }
-            else{
-                nums[i]=0;
-            }
-        }
-        unordered_map<long long,long long>mp={{0,1}};
+        unordered_map<int,long long>mp;
+        mp.insert({0,1});
         long long sum=0;
         long long count=0;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
+            sum+=(nums[i]%modulo==k)?1:0;
             sum%=modulo;
             int need=(sum-k+modulo)%modulo;
             count+=mp[need];
