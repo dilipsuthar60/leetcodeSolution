84b71557255c500bbe1879a1543c1791326903ef
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-11-16 07:31:18 +0530

    Time: 0 ms (100.00%), Space: 33.2 MB (32.15%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -4,12 +4,16 @@ public:
         if(k==1){
             return nums;
         }
-        int count=1;
         int n=nums.size();
+        int count=1;
         vector<int>ans;
         for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]+1) count++;
-            else count=1;
+            if(nums[i-1]+1==nums[i]){
+                count++;
+            }
+            else{
+                count=1;
+            }
             if(i>=k-1){
                 ans.push_back(count>=k?nums[i]:-1);
             }
