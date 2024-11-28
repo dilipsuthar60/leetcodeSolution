e2099e233b7a05efe11550b30e15951cd1d629d5
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-11-28 09:16:25 +0530

    Time: 317 ms (50.93%), Space: 149.7 MB (48.83%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& mat) {
+         vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
+        int n=mat.size();
+        int m=mat[0].size();
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        int dp[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                dp[i][j]=1e9;
+            }
+        }
+        dp[0][0]=0;
+        while(q.size())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                auto temp=q.front();
+                q.pop();
+                int x=temp.first;
+                int y=temp.second;
+                for(auto it:d)
+                {
+                    int nx=x+it.first;
+                    int ny=y+it.second;
+                    if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[x][y]+mat[nx][ny]<dp[nx][ny])
+                    {
+                        dp[nx][ny]=dp[x][y]+mat[nx][ny];
+                         q.push({nx,ny});
+                    }
+                }
+            }
+        }
+        return dp[n-1][m-1];
+                
+    }
+};
