e5e34bb5cd31d90366f75354b6beecd3997547eb
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-11-30 20:48:14 +0530

    Time: 11 ms (14.58%), Space: 17 MB (9.01%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -4,23 +4,10 @@ public:
         int n=str1.size();
         int m=str2.size();
         int i=0,j=0;
-        while(i<n&&j<m){
-            if(str1[i]==str2[j]){
+        for(;i<n&&j<m;i++){
+            if(str1[i]==str2[j]||(str1[i]+1==str2[j])||(str1[i]-25==str2[j])){
                 j++;
             }
-            else{
-                int index1=(str1[i]-'a');
-                int index2=(str2[j]-'a');
-                cout<<(index1+1)%26<<endl;
-                cout<<(index1+25)%26<<endl;
-                if(((index1+1)==index2)){
-                    j++;
-                }
-                if(index1==25&&(index2==0)){
-                    j++;
-                }
-            }
-            i++;
         }
         return j==str2.size();
     }
