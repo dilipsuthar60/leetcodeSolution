db2da1bdec8d725254f7851709a1a3d3598c6ea1
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2024-12-18 22:02:01 +0530

    Time: 3 ms (69.41%), Space: 11.4 MB (57.41%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int nthUglyNumber(int n) 
-    {
-      if(n==1)
-        return 1;
-      int p2,p3,p5;
-      p2=p3=p5=0;
-      int a[n];
-      a[0]=1;
-      for(int i=1;i<n;i++)
-      {
-        a[i]=min(a[p2]*2,min(a[p3]*3,a[p5]*5));
-        if(a[i]==a[p2]*2)
-          p2++;
-        if(a[i]==a[p3]*3)
-          p3++;
-        if(a[i]==a[p5]*5)
-          p5++;
-      }
-      return a[n-1];
+    int nthUglyNumber(int n) {
+        if(n==1) return 1;
+        vector<int>nums(n,0);
+        nums[0]=1;
+        int p2,p3,p5;
+        p2=p3=p5=0;
+        for(int i=1;i<n;i++){
+            int currentValue=min({nums[p2]*2,nums[p3]*3,nums[p5]*5});
+            if(currentValue==nums[p2]*2){
+                p2++;
+            }
+            if(currentValue==nums[p3]*3){
+                p3++;
+            }
+            if(currentValue==nums[p5]*5){
+                p5++;
+            }
+            nums[i]=currentValue;
+        }
+        return nums.back();
     }
 };
